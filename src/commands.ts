e26b2ec91b48c20c5984474f61feddef749206fc
jugglingthebits <stephan.oehlert@gmail.com>
2018-04-16 17:09:11 +0200

    Add clone command

--- src/commands.ts
+++ src/commands.ts
@@ -1,12 +1,25 @@
 import * as yargs from 'yargs';
 import { clone } from './scm';
 
-export async function cloneCommand(args: yargs.Arguments): Promise<void> {
+export function registerCommands(argv: yargs.Argv) {
+    argv.command('clone', 'Clone a contentful space', (args: yargs.Argv) => {
+        args.option('spaceId', {
+            type: 'string',
+            demandOption: true
+        });
+        args.option('contentfulManagementAccessToken', {
+            type: 'string',
+            demandOption: true
+        })
+        return args;
+    }, cloneCommand);
+}
+
+async function cloneCommand(args: yargs.Arguments): Promise<void> {
     const spaceId: string = args.spaceId;
     const contentfulManagementAccessToken: string = args.contentfulManagementAccessToken;
 
     const actualParentPath = 'c:\\workspace';
     await clone(spaceId, actualParentPath, contentfulManagementAccessToken);
+    process.exit(0);
 }
-
-
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import * as yargs from 'yargs';
+import { registerCommands } from './commands';
 
 export async function main() {
     const env = yargs.env();
+    registerCommands(env);
     env.version(false)
         .demandCommand()
         .parse();
