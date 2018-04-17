6b0d5260643bdb83b4f0123841569902f9996007
jugglingthebits <stephan.oehlert@gmail.com>
2018-04-17 16:06:09 +0200

    Refactor into separate functions

--- src/scm.ts
+++ src/scm.ts
@@ -11,12 +11,6 @@ export async function clone(spaceId: string, parentPath: string, accessToken: st
     const hiddenDataPath = path.join(workingCopyPath, '.contentful-scm');
     fs.mkdirSync(hiddenDataPath);
 
-    const entriesPath = path.join(hiddenDataPath, 'entries');
-    fs.mkdirSync(entriesPath);
-
-    const contentTypesPath = path.join(hiddenDataPath, 'content-types');
-    fs.mkdirSync(contentTypesPath);
-
     const config: Config = {
         spaceId,
         accessToken
@@ -27,29 +21,43 @@ export async function clone(spaceId: string, parentPath: string, accessToken: st
     const managementSpace = await managementClient.getSpace(spaceId);
     const masterEnvironment = await managementSpace.getEnvironment('master');
 
+    // TODO: Get more than 100 entries
     const contentTypes = await masterEnvironment.getContentTypes();
-    for (const contentType of contentTypes.items) {
-        const contentTypePath = path.join(contentTypesPath, `${contentType.sys.id}.json`);
-        fs.writeFileSync(contentTypePath, JSON.stringify(contentType));
-    }
-
     // TODO: Get more than 100 entries
     const entries = await masterEnvironment.getEntries();
 
-    for (const entry of entries.items) {
-        const entryPath = path.join(entriesPath, `${entry.sys.id}.json`);
-        fs.writeFileSync(entryPath, JSON.stringify(entry));
-    }
-
-    for (const entry of entries.items) {
-        const entryPath = path.join(workingCopyPath, `${entry.sys.id}.json`);
-        const workingCopyEntry = entry.fields;
-        fs.writeFileSync(entryPath, JSON.stringify(workingCopyEntry, null, 2));
-    }
+    writeHiddenCopy(contentTypes, entries, hiddenDataPath);
+    writeWorkingCopy(contentTypes, entries, workingCopyPath);
 }
 
 function getContentfulManagementClient(accessToken: string) {
     return createManagementClient({
         accessToken
     });
+}
+
+function writeHiddenCopy(contentTypes: any, entries: any, hiddenDataPath) {
+    const contentTypesPath = path.join(hiddenDataPath, 'content-types');
+    fs.mkdirSync(contentTypesPath);
+
+    for (const contentType of contentTypes.items) {
+        const contentTypePath = path.join(contentTypesPath, `${contentType.sys.id}.json`);
+        fs.writeFileSync(contentTypePath, JSON.stringify(contentType));
+    }
+
+    const entriesPath = path.join(hiddenDataPath, 'entries');
+    fs.mkdirSync(entriesPath);
+
+    for (const entry of entries.items) {
+        const entryPath = path.join(entriesPath, `${entry.sys.id}.json`);
+        fs.writeFileSync(entryPath, JSON.stringify(entry));
+    }
+}
+
+function writeWorkingCopy(contentTypes, entries, workingCopyPath) {
+    for (const entry of entries.items) {
+        const entryPath = path.join(workingCopyPath, `${entry.sys.id}.json`);
+        const workingCopyEntry = entry.fields;
+        fs.writeFileSync(entryPath, JSON.stringify(workingCopyEntry, null, 2));
+    }
 }
