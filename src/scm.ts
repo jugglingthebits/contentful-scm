c2475651697223876cdbe2c287d0b5282c02a26b
jugglingthebits <stephan.oehlert@gmail.com>
2018-04-17 15:58:27 +0200

    Create a local copy of all content types

--- src/scm.ts
+++ src/scm.ts
@@ -5,36 +5,46 @@ import { createClient as createManagementClient } from 'contentful-management';
 import { Config, writeConfig } from './config';
 
 export async function clone(spaceId: string, parentPath: string, accessToken: string): Promise<void> {
-    const basePath = path.join(parentPath, spaceId);
-    const hiddenDataPath = path.join(basePath, '.contentful-scm');
-    try {
-        fs.mkdirSync(basePath);
-        fs.mkdirSync(hiddenDataPath);
+    const workingCopyPath = path.join(parentPath, spaceId);
+    fs.mkdirSync(workingCopyPath);
 
-        const config: Config = {
-            spaceId,
-            accessToken
-        };
-        await writeConfig(config, hiddenDataPath);
-    } catch (e) {
-        console.log(e);
-    }
+    const hiddenDataPath = path.join(workingCopyPath, '.contentful-scm');
+    fs.mkdirSync(hiddenDataPath);
+
+    const entriesPath = path.join(hiddenDataPath, 'entries');
+    fs.mkdirSync(entriesPath);
+
+    const contentTypesPath = path.join(hiddenDataPath, 'content-types');
+    fs.mkdirSync(contentTypesPath);
+
+    const config: Config = {
+        spaceId,
+        accessToken
+    };
+    await writeConfig(config, hiddenDataPath);
 
     const managementClient = getContentfulManagementClient(accessToken);
     const managementSpace = await managementClient.getSpace(spaceId);
     const masterEnvironment = await managementSpace.getEnvironment('master');
+
+    const contentTypes = await masterEnvironment.getContentTypes();
+    for (const contentType of contentTypes.items) {
+        const contentTypePath = path.join(contentTypesPath, `${contentType.sys.id}.json`);
+        fs.writeFileSync(contentTypePath, JSON.stringify(contentType));
+    }
+
     // TODO: Get more than 100 entries
     const entries = await masterEnvironment.getEntries();
 
     for (const entry of entries.items) {
-        const entryFilePath = path.join(hiddenDataPath, `${entry.sys.id}.json`);
-        fs.writeFileSync(entryFilePath, JSON.stringify(entry));
+        const entryPath = path.join(entriesPath, `${entry.sys.id}.json`);
+        fs.writeFileSync(entryPath, JSON.stringify(entry));
     }
 
     for (const entry of entries.items) {
-        const workingCopyEntryPath = path.join(basePath, `${entry.sys.id}.json`);
+        const entryPath = path.join(workingCopyPath, `${entry.sys.id}.json`);
         const workingCopyEntry = entry.fields;
-        fs.writeFileSync(workingCopyEntryPath, JSON.stringify(workingCopyEntry, null, 2));
+        fs.writeFileSync(entryPath, JSON.stringify(workingCopyEntry, null, 2));
     }
 }
 
